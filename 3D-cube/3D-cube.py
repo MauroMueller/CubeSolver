9bd0d18fa23510f45f4e80fc9efc5bb8feb70aa2
MauroMueller <104684354+MauroMueller@users.noreply.github.com>
2022-05-05 01:07:04 +0100

    Updated 3D-cube.py: better scope organisation, button-class

--- 3D-cube/3D-cube.py
+++ 3D-cube/3D-cube.py
@@ -3,42 +3,112 @@ import numpy as np
 import math
 import time
 
+pygame.init()
+
+GREY = (150, 150, 150)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 100, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+LINE_THICKNESS = 2
+
+def get_screen_size():
+    try:
+        WIDTH, HEIGHT = pygame.display.get_surface().get_size()
+    except:
+        WIDTH, HEIGHT = 500, 500
+    CENTER = WIDTH/2, HEIGHT/2
+    SCALE = min(WIDTH, HEIGHT)/12
+    return WIDTH, HEIGHT, CENTER, SCALE
+
+WIDTH, HEIGHT, CENTER, SCALE = get_screen_size()
+
+angles = (0, 0, 0)
+
+class Button():
+    def __init__(self, screen, resizeable, x, y, width, height, text, action, color=GREY, fontname="Arial", fontsize=0, textcolor=BLACK):
+        self.screen = screen
+        self.resizeable = resizeable
+        if self.resizeable:
+            self.x = x / WIDTH
+            self.y = y / HEIGHT
+            self.width = width / WIDTH
+            self.height = height / HEIGHT
+        else:
+            self.x = x
+            self.y = y
+            self.width = width
+            self.height = height
+        self.text = text
+        self.action = action
+        self.color = color
+        self.fontname = fontname
+        self.fontsize = fontsize
+        self.textcolor = textcolor
+
+    def calc_coords(self):
+        if self.resizeable:
+            x = self.x * WIDTH
+            y = self.y * HEIGHT
+            width = self.width * WIDTH
+            height = self.height * HEIGHT
+        else:
+            x, y, width, height = self.x, self.y, self.width, self.height
+        if x < 0:
+            x += WIDTH
+        if y < 0:
+            y += HEIGHT
+        return x, y, width, height
+
+    def draw(self):
+        x, y, width, height = self.calc_coords()
+
+        if self.fontsize:    
+            fontsize = self.fontsize
+        else:
+            fontsize = int(0.8 * height)
+        
+        font = pygame.font.SysFont(self.fontname, fontsize) 
+        self.textrender = font.render(self.text, True, self.textcolor)
+
+        if not width:
+            width = 0.5 * height + self.textrender.get_width()
+
+        pygame.draw.rect(self.screen, self.color, (x, y, width, height))
+
+        self.screen.blit(self.textrender, (x+0.25*height, y+(height-fontsize)/2))
+
+    def pressed(self, mouse):
+        x, y, width, height = self.calc_coords()
+        if not width:
+            width = 0.5 * height + self.textrender.get_width()
+        if x <= mouse[0] <= x+width and y <= mouse[1] <= y+height:
+            button_action(self.action)
+
+def button_action(action):
+    if action == "reset_orientation":
+        global angles
+        angles = (0, 0, 0)
+
 def main():
-
-    WIDTH, HEIGHT = 500, 500
-
-    GREY = (150, 150, 150)
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    YELLOW = (255, 255, 0)
-    RED = (255, 0, 0)
-    ORANGE = (255, 100, 0)
-    BLUE = (0, 0, 255)
-    GREEN = (0, 255, 0)
+    global WIDTH, HEIGHT, CENTER, SCALE, angles
 
     cube_vertices = [[[[x, y, z] for x in range(-3, 4, 2)] for y in range(-3, 4, 2)] for z in range(-3, 4, 2)]
-
 
     projection_matrix = [[1, 0, 0], [0, 1, 0]]
 
     motion_matrix = [[0.0, -0.02], [0.02, 0], [0.0, 0.0]]
 
-    LINE_THICKNESS = 2
-
-    def get_screen_size():
-        WIDTH, HEIGHT = pygame.display.get_surface().get_size()
-        CENTER = WIDTH/2, HEIGHT/2
-        SCALE = min(WIDTH, HEIGHT)/12
-        return WIDTH, HEIGHT, CENTER, SCALE
-
-    pygame.init()
-
-
     pygame.display.set_caption("3D Cube")
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     WIDTH, HEIGHT, CENTER, SCALE = get_screen_size()
 
-    angles = (0, 0, 0)
+    # init buttons
+    orient_button = Button(screen, True, 20, -40, 0, 20, "reset cube orientation", "reset_orientation")
 
     running = True
     while running:
@@ -55,8 +125,7 @@ def main():
                 motion = np.dot(motion_matrix, motion)
                 angles = (angles + motion) % (2*math.pi)
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if 20 <= pygame.mouse.get_pos()[0] <= 170 and HEIGHT-40 <= pygame.mouse.get_pos()[1] <= HEIGHT-20:  # button reset cube orientation
-                    angles = (0, 0, 0)
+                orient_button.pressed(pygame.mouse.get_pos())
 
 
         rotation_matrix_x = [[1, 0, 0], [0, math.cos(angles[0]), -math.sin(angles[0])], [0, math.sin(angles[0]), math.cos(angles[0])]]
@@ -147,18 +216,12 @@ def main():
             pygame.draw.line(screen, BLACK, cube_vertices_projected[0][1][3], cube_vertices_projected[3][1][3], LINE_THICKNESS)
             pygame.draw.line(screen, BLACK, cube_vertices_projected[2][3][3], cube_vertices_projected[2][0][3], LINE_THICKNESS)
             pygame.draw.line(screen, BLACK, cube_vertices_projected[3][2][3], cube_vertices_projected[0][2][3], LINE_THICKNESS)
-            
 
-        # button (reset cube orientation)
-        pygame.draw.rect(screen, GREY, (20, HEIGHT-40, 150, 20))
-
-        smallfont = pygame.font.SysFont('Corbel', 20) 
-        text = smallfont.render('reset cube orientation', True, BLACK)
-        
-        screen.blit(text, (25, HEIGHT-35))
+        # draw buttons
+        orient_button.draw()
 
         pygame.display.update()
-        print(angles)
+        #print(angles)
 
     pygame.quit()
 
